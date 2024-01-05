4ab23879ff623823cfdf1cebdd53af77296fbd72
Jacob Fashanu <fashanujacob@gmail.com>
2024-01-05 15:58:02 -0500

    fix main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
@@ -9,9 +8,9 @@ import './App.css';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>,
+  // </React.StrictMode>,
 )
