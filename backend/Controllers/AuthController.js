37b170bbd24e50e9bff985e144b706041cdadfd2
Jacob Fashanu <fashanujacob@gmail.com>
2024-01-07 16:31:34 -0500

    undo last commit

--- backend/Controllers/AuthController.js
+++ backend/Controllers/AuthController.js
@@ -51,9 +51,9 @@ module.exports.Login = async (req, res, next) => {
        const token = createSecretToken(user._id);
        res.cookie("token", token, {
          withCredentials: true,
-        //  httpOnly: false,
-        httpOnly: true,
-         samesite: "none"
+         httpOnly: false,
+        // httpOnly: true,
+        //  samesite: "none"
        });
        res.status(201).json({ message: "User logged in successfully", success: true });
        next()
